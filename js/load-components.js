5fbca2e7ea5d15ca12c3a582ddf3ecf38ce4083f
Богдан Косев <bogdan_gamer@abv.bg>
2023-01-05 20:31:53 +0200

    Extract common code to components, small UI changes

---
+++ js/load-components.js
@@ -0,0 +1,41 @@
+const head = document.getElementsByTagName("head")[0];
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+const aside = document.getElementById("genre-nav");
+
+fetch("./components/head.html")
+	.then((result) => result.text())
+	.then((data) => {
+		head.innerHTML += data;
+	});
+
+fetch("./components/header.html")
+	.then((result) => result.text())
+	.then((data) => {
+		header.innerHTML = data;
+	});
+
+fetch("./components/footer.html")
+	.then((result) => result.text())
+	.then((data) => {
+		footer.innerHTML = data;
+	});
+
+if (aside !== undefined) {
+	fetch("./components/genre-nav.html")
+		.then((result) => result.text())
+		.then((data) => {
+			aside.innerHTML = data;
+		});
+}
+
+const headerScript = document.createElement("script");
+headerScript.src = "./js/header.js";
+headerScript.type = "module";
+
+const dbScript = document.createElement("script");
+dbScript.src = "./js/database.js";
+dbScript.type = "module";
+
+head.appendChild(headerScript);
+head.appendChild(dbScript);
