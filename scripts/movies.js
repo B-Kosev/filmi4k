3166521aa94beb64e5f9f229eee5c178116258ba
Богдан Косев <bogdan_gamer@abv.bg>
2023-01-05 14:11:25 +0200

    Add rating and more info to movie, add filters and movies, improve layout

--- scripts/movies.js
+++ scripts/movies.js
@@ -16,15 +16,10 @@ get(moviesRef).then((info) => {
 		<figure class="fig">
 			<img src=".${movie.imageUrl}" />
             <figcaption>${movie.title} (${movie.year})</figcaption>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
 		</figure>
 	</a>
     </section>`;
-	for (var i = 1; i <= movie.rating; i++) {
-		document.getElementsByClassName("fa-star")[i - 1].classList.add("checkedStar");
-	}
+	// for (var i = 1; i <= movie.rating; i++) {
+	// 	document.getElementsByClassName("fa-star")[i - 1].classList.add("checkedStar");
+	// }
 });
