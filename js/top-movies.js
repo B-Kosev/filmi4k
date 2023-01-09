747b01ad18fe9da3ac55bc7c35e528de24d52c04
Богдан Косев <bogdan_gamer@abv.bg>
2023-01-09 22:08:11 +0200

    small fixes

--- js/top-movies.js
+++ js/top-movies.js
@@ -15,10 +15,10 @@ for (let i = 0; i < numberOfMovies; i++) {
     <section class="cardTop" id="position${i + 1}">
 		<a href="movie.html?title=${movies[i].id}">
 			<span id="position">${i + 1}</span>
-				<div id="images">
-                    <img id="poster" src="${movies[i].posterUrl}" />
-					<img id="screenshot" src="${movies[i].screenshotUrl}" />
-				</div>
+			<div id="images">
+				<img id="poster" src="${movies[i].posterUrl}" />
+				<img id="screenshot" src="${movies[i].screenshotUrl}" />
+			</div>
 			<span>${movies[i].title_en}</span>
 			<span>${movies[i].title_bg}</span>
 			<span>${movies[i].year}</span>
