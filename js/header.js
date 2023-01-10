0d91d3d604a8fec29da05feb2042aad9d390eba4
toshko <toshko_stamatov@abv.bg>
2023-01-10 22:17:12 +0200

    Fix login and header buttons.

--- js/header.js
+++ js/header.js
@@ -1,17 +1,19 @@
-if (localStorage.getItem("username")) {
-	document.getElementById("login-parent").classList.add("hidden");
-	document.getElementById("register-parent").classList.add("hidden");
-	document.getElementById("greeting-parent").classList.remove("hidden");
-	document.getElementById("greeting").innerText = localStorage.getItem("username");
-	document.getElementById("logout-button-parent").classList.remove("hidden");
-} else {
-	document.getElementById("login-parent").classList.remove("hidden");
-	document.getElementById("register-parent").classList.remove("hidden");
-	document.getElementById("greeting-parent").classList.add("hidden");
-	document.getElementById("logout-button-parent").classList.add("hidden");
-}
+window.onload = function () {
+	if (localStorage.getItem("username")) {
+		document.getElementById("login-parent").classList.add("hidden");
+		document.getElementById("register-parent").classList.add("hidden");
+		document.getElementById("greeting-parent").classList.remove("hidden");
+		document.getElementById("greeting").innerText = localStorage.getItem("username");
+		document.getElementById("logout-button-parent").classList.remove("hidden");
+	} else {
+		document.getElementById("login-parent").classList.remove("hidden");
+		document.getElementById("register-parent").classList.remove("hidden");
+		document.getElementById("greeting-parent").classList.add("hidden");
+		document.getElementById("logout-button-parent").classList.add("hidden");
+	}
 
-document.getElementById("logout-button").addEventListener("click", () => {
-	localStorage.removeItem("username");
-	window.location.replace("./index.html");
-});
+	document.getElementById("logout-button").addEventListener("click", () => {
+		localStorage.removeItem("username");
+		window.location.replace("./index.html");
+	});
+};
--- js/login.js
+++ js/login.js
@@ -1,29 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, set, push, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-import { firebaseConfig } from "./database.js";
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-window.onload = function () {
-	document.getElementById("login-form").addEventListener("submit", (event) => loginUser(event));
-};
+import { getUser } from "./database.js";
 
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
-const loginUser = (event) => {
+const loginUser = async (event) => {
 	event.preventDefault();
-	const usersRef = ref(database, "users/" + username.value);
 	document.getElementById("credentials-err").classList.add("hidden");
+	const user = await getUser(username.value);
 
-	get(usersRef).then((info) => {
-		const user = info.val();
-		if (!user || user.password != password.value) {
-			document.getElementById("credentials-err").classList.remove("hidden");
-		} else {
-			localStorage.setItem("username", `${username.value}`);
-			window.location.replace("./movies.html");
-		}
-	});
+	if (!user || user.password != password.value) {
+		document.getElementById("credentials-err").classList.remove("hidden");
+	} else {
+		localStorage.setItem("username", `${username.value}`);
+		window.location.replace("./movies.html");
+	}
 };
+
+document.getElementById("login-form").addEventListener("submit", (event) => loginUser(event));
